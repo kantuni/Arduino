7696cda4792afd5a4cc6216c44f423ebdce72ad8
kantuni <henrikh.kantuni@gmail.com>
2018-03-17 21:26:49 -0400

    Update lights.ino

--- lights/lights.ino
+++ lights/lights.ino
@@ -5,19 +5,14 @@ const int RECEIVER_PIN = 15;
 IRrecv irrecv(RECEIVER_PIN);
 decode_results results;
 
-
-int p4 = 4, p5 = 5, p6 = 6, p7 = 7,
-    p8 = 8, p9 = 9, p10 = 10, p11 = 11;
-int aa1, aa2, aa3, aa4, aa5;
+int pins[8] = {4, 5, 6, 7, 8, 9, 10, 11};
 bool d = false;
 
 int brightness = 255;
 int fadeAmount = 5;
-bool turnOff = false;
+bool turnOn = false;
 
 
-// fade out functionality (provide pin)
-// start
 void fadeOut(int pin) {
   // set the brightness of the pin
   analogWrite(pin, brightness);
@@ -27,39 +22,28 @@ void fadeOut(int pin) {
 
   if (brightness <= 0) {
     // switch back to digital
-    digitalWrite(p9, LOW);
+    digitalWrite(pins[5], LOW);
     // finish turn off process
-    turnOff = false;
+    turnOn = false;
   }
 
   // wait for 20 ms to see the dimming effect
   delay(20);
 }
-// end
 
 void setup() {
   // enable receiver decoding
   irrecv.enableIRIn();
 
-  aa1 = 0;
-  aa2 = 0;
-  aa3 = 0;
-  aa4 = 0;
-  aa5 = 0;
-
-  pinMode(p4, OUTPUT);
-  pinMode(p5, OUTPUT);
-  pinMode(p6, OUTPUT);
-  pinMode(p7, OUTPUT);
-  pinMode(p8, OUTPUT);
-  pinMode(p9, OUTPUT);
-  pinMode(p10, OUTPUT);
-  pinMode(p11, OUTPUT);
+  // configure the pins to behave as OUTPUTs
+  for (int i = 0; i < 8; i++) {
+    pinMode(pins[i], OUTPUT);
+  }
 }
 
 void loop() {
   // if turn off process is on
-  if (turnOff) {
+  if (turnOn) {
     fadeOut(p9);
   }
 
@@ -68,52 +52,28 @@ void loop() {
       // on
       case 0xFD00FF: {
         // toggle lights
-        digitalWrite(p5, !digitalRead(p5));
-        digitalWrite(p6, !digitalRead(p6));
+        digitalWrite(pins[1], !digitalRead(pins[1]));
+        digitalWrite(pins[2], !digitalRead(pins[2]));
         break;
       }
       // func / stop
       case 0xFD40BF: {
-        if (aa2 == 0) {
-          digitalWrite(p4, HIGH);
-          aa2 = 1;
-        } else {
-          digitalWrite(p4, LOW);
-          aa2 = 0;
-        }
+        digitalWrite(pins[0], !digitalRead(pins[0]));
         break;
       }
       // rewind
       case 0xFD20DF: {
-        if (aa3 == 0) {
-          digitalWrite(p8, HIGH);
-          aa3 = 1;
-        } else {
-          digitalWrite(p8, LOW);
-          aa3 = 0;
-        }
+        digitalWrite(pins[4], !digitalRead(pins[4]));
         break;
       }
       // forward
       case 0xFD609F: {
-        if (aa4 == 0) {
-          digitalWrite(p9, HIGH);
-          aa4 = 1;
-        } else {
-          digitalWrite(p9, LOW);
-          aa4 = 0;
-        }
+        digitalWrite(pins[5], !digitalRead(pins[5]));
         break;
       }
       // play
       case 0xFDA05F: {
-        if (aa5 == 0) {
-          digitalWrite(p10, HIGH);
-          aa5 = 1;
-        } else {
-          digitalWrite(p10, LOW);
-          aa5 = 0;
-        }
+        digitalWrite(pins[6], !digitalRead(pins[6]));
         break;
       }
       // volume up
@@ -126,17 +86,18 @@ void loop() {
         if (digitalRead(p9) == LOW) {
           digitalWrite(p9, HIGH);
         } else {
-          turnOff = true;
+          turnOn = true;
           brightness = 255;
         }
         break;
       }
     }
 
+    // resume recieving commands
     irrecv.resume();
   }
 
-  if (d == true) {
+  if (d) {
     digitalWrite(p7, HIGH);
     delay(500);
     digitalWrite(p7, LOW);
